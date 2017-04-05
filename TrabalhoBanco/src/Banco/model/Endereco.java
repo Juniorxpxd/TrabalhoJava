2341f6b02f48256ae380a5f4892d0e555cb0297b
Aluno <Aluno@050LABE031.up.edu>
2017-04-05 10:05:59 -0300

    asdas

--- TrabalhoBanco/src/Banco/model/Endereco.java
+++ TrabalhoBanco/src/Banco/model/Endereco.java
@@ -1,5 +1,53 @@
 package Banco.model;
 
 public class Endereco {
+	
+	private String rua;
+	private int numero;
+	private String bairro;
+	private String cidade;
+	private String estado; 
+	private String pais;
+	
+	public String getRua() {
+		return rua;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	
+	
+	
+	
 
 }
--- TrabalhoBanco/src/Banco/model/Pessoa.java
+++ TrabalhoBanco/src/Banco/model/Pessoa.java
@@ -1,5 +1,24 @@
 package Banco.model;
 
 public class Pessoa {
+	
+	private String fisica;
+	private String juridica;
+	
+	public String getFisica() {
+		return fisica;
+	}
+	public String getJuridica() {
+		return juridica;
+	}
+	public void setFisica(String fisica) {
+		this.fisica = fisica;
+	}
+	public void setJuridica(String juridica) {
+		this.juridica = juridica;
+	}
+	
+	
+	
 
 }
