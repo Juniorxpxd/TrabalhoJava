1ab05f23fe15589b3caea900da4d479965ece98c
Aluno <Aluno@050LAB000X>
2017-04-25 11:10:46 -0300

    x

---
+++ TrabalhoBanco/src/Banco/dal/DepositoDAO.java
@@ -0,0 +1,53 @@
+package Banco.dal;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.RollbackException;
+
+import Banco.model.Deposito;
+import Banco.model.Pessoa;
+
+public class DepositoDAO {
+	private static ArrayList<Deposito> deposito = new ArrayList<Deposito>();
+	public static void adicionarDeposito(Deposito d){
+		EntityManager em = Conexao.getEntityManager();
+		em.getTransaction().begin();
+		em.persist(d);
+		em.getTransaction().commit();
+		em.close();
+	}
+	public static void removerPessoa(Deposito d){
+		try{
+			EntityManager em = Conexao.getEntityManager();
+			em.getTransaction().begin();
+			d = em.getReference(Pessoa.class, d.get());
+			em.remove(d);
+			em.getTransaction().commit();
+			em.close();
+			}catch(RollbackException d){
+				System.out.println(d.toString());
+			}
+		}
+	public static List<Deposito> retornarLista(){
+		EntityManager em = Conexao.getEntityManager();
+		Query q = em.createQuery("SELECT d FROM Deposito d");
+		List<Deposito> lista = q.getResultList();
+		em.close();
+		return lista;
+	}
+	public static void alterarDeposito(Deposito d){
+		EntityManager em = Conexao.getEntityManager();
+		em.getTransaction().begin();
+		Deposito deposito = em.find(Deposito.class, d.getIdDeposito());
+		deposito.setQuantidade(d.getQuantidade());
+		
+		em.merge(deposito);
+		em.getTransaction().commit();
+		em.close();
+	}
+	public static Deposito buscarDepositoPorId(int id){
+		EntityManager em = Conexao.getEntityManager();
+}
+}
